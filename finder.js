b6956bc9d4b657fe9850d587513c5be3fcec4505
drshariqalikhan <31785258+drshariqalikhan@users.noreply.github.com>
2017-09-10 20:09:42 +0800

    Update finder.js

--- finder.js
+++ finder.js
@@ -5,8 +5,27 @@
 'use strict';
 module.exports =
     {
-        locater:function (jason)
+        locater:function (st)
         {
+          switch(st) {
+          case "gut_ai.Hi":
+              return "ai.HI intent";
+              break;
+          case "gut_ai.Hi - yes":
+              return "ai.HI intent-yes";
+              break;
+          case "gut_ai.Hi - no":
+             return "ai.HI intent-no";
+              break;
+          case "WEather":
+             return "weather intent";
+              break;
+          case "magic":
+              return "magic intent";
+              break;
+          default:
+               return "??"+ state;
+     } 
 
         }
     };
