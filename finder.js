5524715fe407a7bc55d4785bb5176ab31b358b7f
drshariqalikhan <31785258+drshariqalikhan@users.noreply.github.com>
2017-09-10 20:09:57 +0800

    Update finder.js

--- finder.js
+++ finder.js
@@ -24,7 +24,7 @@ module.exports =
               return "magic intent";
               break;
           default:
-               return "??"+ state;
+               return "??"+ st;
      } 
 
         }
