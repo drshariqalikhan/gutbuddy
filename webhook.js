3c42c991610af9a4f6e8a750932f2cb96b82bfa7
drshariqalikhan <31785258+drshariqalikhan@users.noreply.github.com>
2017-09-18 11:32:42 +0800

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fd = require('./finder.js');
 const rsp = require('./responses.js');
+const xl = require('./xlwrite.js');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,7 +13,7 @@ app.use(bodyParser.json());
 
 
 app.post('/echo', function(req, res) {
-    
+    xl.record(200);
     //detect intent
     var output = fd.responder(req.body.result.metadata.intentName);
   
